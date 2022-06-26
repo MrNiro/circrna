f324793c8f4a3ca95d105fcbb4fbd093b4281be8
Niro Lin <linqinyun@outlook.com>
2022-06-26 12:12:36 -0400

    Add a script to create input sample

---
+++ utils/create_input.py
@@ -0,0 +1,31 @@
+import sys
+import os
+
+
+if __name__ == '__main__':
+    data_folder = "/home/ubuntu/data/"
+
+    if len(sys.argv) > 2:
+        data_folder = sys.argv[1]
+
+    input_sample = open("/home/ubuntu/circrna/sample.csv", "w")
+    input_sample.write("Sample_ID,Read1,Read2,Bam\n")
+    for _, dirs, _ in os.walk(data_folder):
+        for d in dirs:
+            line = d + ","
+            cur_path = data_folder + d
+            for dpath, _, filenames in os.walk(cur_path):
+                for f_name in filenames:
+                    if "MD5" in f_name:
+                        filenames.remove(f_name)
+
+                if len(filenames) == 2:
+                    line += (cur_path + filenames[0] + ",")
+                    line += (cur_path + filenames[1] + ",NA\n")
+                else:
+                    Exception("Missing files in " + cur_path)
+                break
+
+            input_sample.write(line)
+
+    input_sample.close()
