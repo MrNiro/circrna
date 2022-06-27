f6ab8e4e685140626703914be06d1f1457e2451e
Niro Lin <linqinyun@outlook.com>
2022-06-27 16:54:06 -0400

    update input creating script

--- utils/create_input.py
+++ utils/create_input.py
@@ -8,14 +8,14 @@ if __name__ == '__main__':
     if len(sys.argv) > 2:
         data_folder = sys.argv[1]
 
-    input_sample = open("/home/ubuntu/circrna/sample.csv", "w")
+    input_sample = open("/home/ubuntu/circrna/samples.csv", "w")
     input_sample.write("Sample_ID,Read1,Read2,Bam\n")
     for _, dirs, _ in os.walk(data_folder):
-        for d in dirs:
+        for d in dirs.sort():
             line = d + ","
             cur_path = data_folder + d
             for dpath, _, filenames in os.walk(cur_path):
-                for f_name in filenames:
+                for f_name in filenames.sort():
                     if "MD5" in f_name:
                         filenames.remove(f_name)
 
