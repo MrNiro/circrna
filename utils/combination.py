1c553b0fbe394b4649a0c7832fc16c8bfd19273a
Niro Lin <linqinyun@outlook.com>
2022-07-27 14:14:58 -0400

    add code to combine DEA results together

--- utils/combination.py
+++ utils/combination.py
@@ -1,6 +1,51 @@
 import csv
 import os
 
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+chrome_option = webdriver.ChromeOptions()
+chrome_option.add_argument(argument='--headless')
+browser = webdriver.Chrome(options=chrome_option)   # options=chrome_option
+wait = WebDriverWait(browser, 2)  # 最长等待时间为3S
+# wait_v = WebDriverWait(browser, 20)
+browser.maximize_window()
+
+
+def crawler(g_name):
+    if g_name == "na":
+        return "unknown_type"
+
+    base_url = "https://www.genecards.org/cgi-bin/carddisp.pl?gene="
+    url = base_url + g_name
+    browser.get(url)
+    print("Searching:", g_name, end="...")
+    try:
+        g_type = wait.until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, "#aliases_descriptions > div.row "
+                              "> div.col-xs-12.col-md-9 > div:nth-child(2) > div > div"))).text
+    except TimeoutException:
+        try:
+            temp = wait.until(EC.presence_of_element_located(
+                (By.CSS_SELECTOR, "#cardPage > div:nth-child(1) > div > div > div.gc-section-header.gc-first "
+                                  "> div.col-xs-8.col-md-10.col-sm-9.gene-symbol-description.row-no-padding "
+                                  "> div > span.gc-category"))).text
+            if temp == "Protein Coding":
+                g_type = "protein_coding"
+            elif temp == "Pseudogene":
+                g_type = "pseudogene"
+            else:
+                print("----------------------------------", temp)
+                g_type = temp
+
+        except TimeoutException:
+            g_type = "unknown_type"
+    print("\t\ttype: ", g_type)
+    return g_type
+
 
 def combine_bed():
     all_bed = {}
@@ -19,7 +64,7 @@ def combine_bed():
                         rna_type = info[12]
                         rna_name = info[13]
                         if rna_id not in all_bed:
-                            all_bed[rna_id] = (rna_type, rna_name)
+                            all_bed[rna_id] = [rna_type, rna_name]
     return all_bed
 
 
@@ -50,8 +95,19 @@ def load_circrna(all_bed):
     for each in all_rna.keys():
         if each in all_bed:
             all_rna[each].append(all_bed[each])
-        print(all_rna[each])
+        # print(all_rna[each])
     return all_rna, diff_types
+
+
+def load_gene_type():
+    gene_type_file = open("./my_genbe_type.txt")
+    gene_type = {}
+    for line in gene_type_file.readlines():
+        g_name, g_type = line.strip().split(" ")
+        gene_type[g_name] = g_type
+    gene_type_file.close()
+
+    return gene_type
 
 
 def generate_csv(all_rna, diff_types):
@@ -59,13 +115,35 @@ def generate_csv(all_rna, diff_types):
     csv_title = "circrna_ID,"
     for each in diff_types:
         csv_title += ("log2FoldChange_%s,p_value_%s," % (each, each))
-    csv_title += "circType,geneName,geneName2,geneName3\n"
+    csv_title += "circType,geneName,geneName2,geneName3,geneType\n"
     combine_result.write(csv_title)
 
+    gene_type = load_gene_type()
     for each in all_rna.keys():
         value = all_rna[each]
-        line = "%s,%lf,%lf,%f,%f,%f,%f,%s,%s\n" \
-               % (each, value[0], value[1], value[2], value[3], value[4], value[5], value[6][0], value[6][1])
+
+        g_type = "unknown_type"
+        if "," in value[6][1]:
+            g_names = value[6][1].split(",")
+            for g_name in g_names:
+                g_name = g_name.lower()
+                if g_name in gene_type:
+                    g_type = gene_type[g_name]
+                    break
+                else:
+                    g_type = crawler(g_name)
+            if len(g_names) == 2:
+                value[6][1] += ","
+        else:
+            g_name = value[6][1].lower()
+            if g_name in gene_type:
+                g_type = gene_type[g_name]
+            else:
+                g_type = crawler(g_name)
+            value[6][1] += ",,"
+
+        line = "%s,%lf,%lf,%f,%f,%f,%f,%s,%s,%s\n" \
+               % (each, value[0], value[1], value[2], value[3], value[4], value[5], value[6][0], value[6][1], g_type)
         combine_result.write(line)
 
     combine_result.close()
